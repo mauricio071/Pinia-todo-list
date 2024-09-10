257a75f1190935e5409036b2ec663c35cf53a518
Maurício Naoki <naokimau@gmail.com>
2024-09-09 22:48:22 -0300

    finalização do projeto

--- src/components/TarefaDetalhes.vue
+++ src/components/TarefaDetalhes.vue
@@ -1,27 +1,57 @@
 <template>
     <div class="task">
-        <h3>{{ tarefa.titulo }}</h3>
+        <button @click="atualizarStatus" class="mr-4">
+            <i :class="{
+                '!text-green-600': completada,
+                '!text-gray-400': !completada
+            }" class="material-icons">check</i>
+        </button>
+        <input v-model="novoTitulo" @keyup.enter="atualizarTitulo" @blur="atualizarTitulo" type="text"
+            placeholder="Digite a sua tarefa"
+            class=" placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3">
         <div class="icons">
-            <i @click="favoritar(tarefa.id)" class="material-icons"
-                :class="{ 'favoritado': tarefa.favoritado }">favorite</i>
-            <i @click=remover(tarefa.id) class="material-icons">delete</i>
+            <div>
+                <i @click="atualizarMarca" class="material-icons" :class="{ 'marcada': marcada }">bookmark</i>
+            </div>
+            <div>
+                <i @click=remover(tarefa.id) class="material-icons">delete</i>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { mapActions } from 'pinia'
+import { ref } from 'vue';
 import { useTarefaStore } from '../stores/TarefaStore'
 
 const { tarefa } = defineProps(['tarefa'])
 
 const tarefaStore = useTarefaStore()
 
-const { favoritar, remover } = mapActions(useTarefaStore, ["favoritar", "remover"])
+const atualizarTarefa = tarefaStore.atualizarTarefa
+const remover = tarefaStore.remover
+
+const novoTitulo = ref(tarefa.titulo)
+const completada = ref(tarefa.completada)
+const marcada = ref(tarefa.marcada)
+
+const atualizarStatus = () => {
+    completada.value = !completada.value
+    atualizarTarefa(tarefa.id, { completada: completada.value })
+}
+
+const atualizarTitulo = () => {
+    atualizarTarefa(tarefa.id, { titulo: novoTitulo.value })
+}
+
+const atualizarMarca = () => {
+    marcada.value = !marcada.value
+    atualizarTarefa(tarefa.id, { marcada: marcada.value })
+}
 </script>
 
 <style scoped>
-.favoritado {
-    color: red
+.marcada {
+    @apply text-blue-500;
 }
 </style>
